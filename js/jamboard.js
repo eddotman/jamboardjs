771f11b674bc37b1458123b3e25cd05ceb5a5abd
Eddie Kim <eddotman@gmail.com>
2013-08-01 16:17:39 -0400

    buttons working

--- js/jamboard.js
+++ js/jamboard.js
@@ -3,6 +3,7 @@
 function Jamboard() {};
 Jamboard.prototype = {
 	key: false,
+	name: null,
 	w: null,
 	h: null,
 
@@ -14,20 +15,31 @@ Jamboard.prototype = {
 
 		for (var i=0 ; i < this.h; i++) { 
 			for (var j=0; j < this.w; j++) {
-				$(".jamboard").append("<button/>");	
+				$(".jamboard").append("<button class='jmb-btn' id='" + i + "-" + j + "' />");	
 			}
 			$(".jamboard").append("<br>");
 		}
 
 		$(".jamboard").append("<br><p>Click jamboard buttons for beats. Use your keyboard to jam.</p>");
 		$(".jamboard").append("<p><em>(jamboard currently not functional)</em></p>");
-	}		
+	},
+
+	activate: function () {
+		$(".jmb-btn").click(function (event) {
+			event.preventDefault();
+			var id = event.target.id;
+			//$("#" + id).css("background-color", "#ff5555");
+			$("#" + id).toggleClass("active");
+		});
+	} 
 };
 
 
 //Generate Jamboard
 $(document).ready(function() {
 	var jmb1 = new Jamboard();
+	jmb1.name = "jmb1";
 	jmb1.key = true;
 	jmb1.genGrid();
+	jmb1.activate();
 });
